15428918d2b481e543a411c7040aff9a4f71157e
ka9cql <ka9cql@gmail.com>
2018-05-27 02:04:06 -0700

    Added FSQ experiment using Si5351 board. Initial checkin of working test code.

---
+++ FSQ/sizeof.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	printf("\n");
+	printf("Sizeof char   : %d\n", sizeof(char  ));
+	printf("Sizeof short  : %d\n", sizeof(short ));
+	printf("Sizeof int    : %d\n", sizeof(int   ));
+	printf("Sizeof long   : %d\n", sizeof(long  ));
+	printf("Sizeof float  : %d\n", sizeof(float ));
+	printf("Sizeof double : %d\n", sizeof(double));
+	printf("\n");
+}
